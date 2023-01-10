c68d0c4be3f8444ce883fb4863da02291b9ee05b
Mac_Developer <araozangel842@gmail.com>
2023-01-10 13:18:05 +0200

    Add main bg image

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -6,9 +6,18 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-        Image.asset('assets/images/flutter-no-bg.png', height: 200, width: 200)
-      ]),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned.fill(
+            child: Image.asset('assets/images/main_bg.jpeg',
+                width: 100, height: 100, fit: BoxFit.cover),
+          ),
+          Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            Image.asset('assets/images/flutter-no-bg.png', height: 150)
+          ])
+        ],
+      ),
     );
   }
 }
