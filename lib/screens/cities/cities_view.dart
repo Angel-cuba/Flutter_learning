6c9fc8344b571da2fb815faba3f6771986ef559e
Mac_Developer <araozangel842@gmail.com>
2023-01-10 22:47:51 +0200

    Cities view

--- lib/screens/cities/cities_view.dart
+++ lib/screens/cities/cities_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/common/header.dart';
+import 'package:weather_app/constants/colors.dart';
+import 'package:weather_app/constants/ui_constants.dart';
 
 class CitiesView extends StatelessWidget {
   const CitiesView({super.key});
@@ -7,10 +10,27 @@ class CitiesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cities'),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+        elevation: 0.1,
       ),
-      body: const Center(
-        child: Text('Cities'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add, color: primaryColor),
+      ),
+      body: ConstrainedBox(
+        constraints: const BoxConstraints(maxWidth: maxPageWidth),
+        child: Column(
+          children: const <Widget>[
+            Header(
+              title: "My cities",
+            ),
+            Expanded(
+                child: Center(
+              child: Text("No cities added yet"),
+            ))
+          ],
+        ),
       ),
     );
   }
