abfca4233f59bce0357d2c8fe18e4a0385e48991
Mac_Developer <araozangel842@gmail.com>
2023-01-10 23:38:45 +0200

    Add styles to the text field

--- lib/screens/cities/add_city_view.dart
+++ lib/screens/cities/add_city_view.dart
@@ -13,12 +13,26 @@ class AddCityView extends StatelessWidget {
         iconTheme: const IconThemeData(color: Colors.black),
         elevation: 0.1,
       ),
-      body: ConstrainedBox(
-        constraints: const BoxConstraints(maxWidth: maxPageWidth),
+      body: Padding(
+        padding: EdgeInsets.all(25),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: const <Widget>[
             Header(
               title: "Add city",
+            ),
+            SizedBox(height: 20),
+            ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              child: TextField(
+                decoration: InputDecoration(
+                    focusedBorder: InputBorder.none,
+                    border: InputBorder.none,
+                    filled: true,
+                    hintText: 'Search city',
+                    fillColor: Color.fromARGB(121, 199, 194, 194),
+                    prefixIcon: Icon(Icons.search, color: Colors.grey)),
+              ),
             ),
           ],
         ),
