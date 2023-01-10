b0426549b44fa3291828c255cde6ac5301f40ee9
Mac_Developer <araozangel842@gmail.com>
2023-01-10 21:44:40 +0200

    Finish first screen view

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,9 +15,50 @@ class HomeScreen extends StatelessWidget {
             child: Image.asset('assets/images/main_bg.jpeg',
                 width: 100, height: 100, fit: BoxFit.cover),
           ),
-          Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-            Image.asset('assets/images/flutter-no-bg.png', height: 150)
-          ])
+          SafeArea(
+              child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 45, vertical: 20),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(height: 20),
+                  Image.asset('assets/images/flutter-no-bg.png', height: 150),
+                  const SizedBox(height: 60),
+                  const Text('Welcome, \nFlutter Developer!',
+                      textAlign: TextAlign.end,
+                      style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87)),
+                  const Center(
+                    child: Text(
+                      'What do you, \nToday!',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.tealAccent),
+                    ),
+                  ),
+                  ButtonBar(
+                    alignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          print('Login');
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue.shade500,
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 70, vertical: 10),
+                            textStyle: const TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                        child: const Text('Login'),
+                      ),
+                    ],
+                  )
+                ]),
+          )),
         ],
       ),
     );
