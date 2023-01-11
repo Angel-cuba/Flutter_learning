577393aaa06644906d4f3c2b7a87d1a53cce075c
Mac_Developer <araozangel842@gmail.com>
2023-01-11 20:50:30 +0200

    Add search input

--- lib/screens/cities/add_city_view.dart
+++ lib/screens/cities/add_city_view.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/common/header.dart';
-import 'package:weather_app/constants/ui_constants.dart';
+// import 'package:weather_app/constants/ui_constants.dart';
 
-class AddCityView extends StatelessWidget {
+class AddCityView extends StatefulWidget {
   const AddCityView({super.key});
 
+  @override
+  State<AddCityView> createState() => _AddCityViewState();
+}
+
+void _findandAddCity(String cityName) {
+  print(cityName);
+}
+
+class _AddCityViewState extends State<AddCityView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,8 +32,9 @@ class AddCityView extends StatelessWidget {
             ),
             SizedBox(height: 20),
             ClipRRect(
-              borderRadius: BorderRadius.all(Radius.circular(radius)),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
               child: TextField(
+                onChanged: _findandAddCity,
                 decoration: InputDecoration(
                     focusedBorder: InputBorder.none,
                     border: InputBorder.none,
