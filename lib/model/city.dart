d792b77b572ebb50f8cb1b54aaa82d69e75f3bee
Mac_Developer <araozangel842@gmail.com>
2023-01-13 18:47:16 +0200

    Ongoing work with new token.

--- lib/model/city.dart
+++ lib/model/city.dart
@@ -1,43 +1,60 @@
+import 'dart:convert';
+
+List<City> cityFromJson(String str) =>
+    List<City>.from(json.decode(str).map((x) => City.fromJson(x)));
+
 class City {
   final String id;
   final String name;
   final String country;
-  final String timezone;
-  final String language;
-  final String state;
-  final String adminArea;
-  final String adminArea2;
-  final String adminArea3;
-  final String lon;
-  final String lat;
+  // final String timezone;
+  // final String language;
+  // final String state;
+  // final String adminArea;
+  // final String adminArea2;
+  // final String adminArea3;
+  // final String lon;
+  // final String lat;
 
   City({
     required this.id,
     required this.name,
     required this.country,
-    required this.timezone,
-    required this.language,
-    required this.state,
-    required this.adminArea,
-    required this.adminArea2,
-    required this.adminArea3,
-    required this.lon,
-    required this.lat,
+    // required this.timezone,
+    // required this.language,
+    // required this.state,
+    // required this.adminArea,
+    // required this.adminArea2,
+    // required this.adminArea3,
+    // required this.lon,
+    // required this.lat,
   });
 
-  factory City.fromJson(Map<String, dynamic> json) {
-    return City(
-      id: json['id'],
-      name: json['name'],
-      country: json['country'],
-      timezone: json['timezone'],
-      language: json['language'],
-      state: json['state'],
-      adminArea: json['adminArea'],
-      adminArea2: json['adminArea2'],
-      adminArea3: json['adminArea3'],
-      lon: json['lon'],
-      lat: json['lat'],
-    );
-  }
+  factory City.fromJson(Map<String, dynamic> json) => City(
+        id: json['id'],
+        name: json['name'],
+        country: json['country'],
+        // timezone: json['timezone'],
+        // language: json['language'],
+        // state: json['state'],
+        // adminArea: json['adminArea'],
+        // adminArea2: json['adminArea2'],
+        // adminArea3: json['adminArea3'],
+        // lon: json['lon'],
+        // lat: json['lat'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'country': country,
+        // 'timezone': timezone,
+        // 'language': language,
+        // 'state': state,
+        // 'adminArea': adminArea,
+        // 'adminArea2': adminArea2,
+        // 'adminArea3': adminArea3,
+        // 'lon': lon,
+        // 'lat': lat,
+      };
 }
--- lib/screens/cities/add_city_view.dart
+++ lib/screens/cities/add_city_view.dart
@@ -1,36 +1,61 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weather_app/common/header.dart';
 import 'package:weather_app/constants/debouncer.dart';
 import 'package:weather_app/constants/ui_constants.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:weather_app/data/data.dart';
-// DotEnv.env['TOKEN']
-
-Future<void> main() async {
-  await dotenv.load();
-  runApp(const AddCityView());
-}
+import 'package:weather_app/model/city.dart';
 
 class AddCityView extends StatefulWidget {
   const AddCityView({super.key});
 
   @override
-  State<AddCityView> createState() => _AddCityViewState();
-}
-
-const TOKEN =
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9wZmEuZm9yZWNhLmNvbVwvYXV0aG9yaXplXC90b2tlbiIsImlhdCI6MTY3MzUwOTI2NywiZXhwIjoxNjczNTk1NjY3LCJuYmYiOjE2NzM1MDkyNjcsImp0aSI6ImNkMWViMGRlYzVhOTgxYzUiLCJzdWIiOiJhbmdlbGl0by1jdWJhMTk4NCIsImZtdCI6IlhEY09oakM0MCtBTGpsWVR0amJPaUE9PSJ9.BUgz-jX2ETXlcqj1-PvwB7CBNQKq2yqKBbWHuzc4EDw';
-final debouncer = Debouncer();
-final token = dotenv.env['API_TOKEN'].toString();
-
-void _findandAddCity(String cityName) {
-  debouncer.run(() {
-    // do something with cityName
-    fetchData(TOKEN, cityName);
-  });
+  _AddCityViewState createState() => _AddCityViewState();
 }
 
 class _AddCityViewState extends State<AddCityView> {
+  final debouncer = Debouncer();
+  List<City> cities = [];
+
+  findAndAddCity(String cityName) {
+    debouncer.run(() {
+      // do something with cityName
+      fetchData(TOKEN, cityName);
+    });
+  }
+
+  void fetchData(String token, String text) async {
+    final response = await http.get(
+        Uri.parse(
+            'https://pfa.foreca.com/api/v1/location/search/$text?lang=es'),
+        headers: {'Authorization': 'Bearer $token'});
+    // final data = json.decode(response.body);
+    // print(data);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        // cities = data.map((json) => City.fromJson(json)).toList();
+        // print(cities);
+        print(data);
+        cities = data.map((json) => City.fromJson(json)).toList().cast<City>();
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (cities.isEmpty) {
+      fetchData(TOKEN, 'Madrid');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,16 +68,16 @@ class _AddCityViewState extends State<AddCityView> {
         padding: const EdgeInsets.all(25),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const <Widget>[
-            Header(
+          children: <Widget>[
+            const Header(
               title: "Add city",
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ClipRRect(
-              borderRadius: BorderRadius.all(Radius.circular(radius)),
+              borderRadius: const BorderRadius.all(Radius.circular(radius)),
               child: TextField(
-                onChanged: _findandAddCity,
-                decoration: InputDecoration(
+                onChanged: findAndAddCity,
+                decoration: const InputDecoration(
                     focusedBorder: InputBorder.none,
                     border: InputBorder.none,
                     filled: true,
@@ -60,6 +85,26 @@ class _AddCityViewState extends State<AddCityView> {
                     fillColor: Color.fromARGB(121, 199, 194, 194),
                     prefixIcon: Icon(Icons.search, color: Colors.grey)),
               ),
+            ),
+            const SizedBox(height: 20),
+            ListView.separated(
+              shrinkWrap: true,
+              itemCount: cities.length,
+              itemBuilder: (BuildContext context, int index) {
+                final city = cities[index];
+                return city == null
+                    ? const Center(child: CircularProgressIndicator())
+                    : ListTile(
+                        title: Text(city.name),
+                        subtitle: Text(city.country),
+                        // onTap: () {
+                        //   Navigator.pop(context, cities[index]);
+                        // },
+                      );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return const Divider();
+              },
             ),
           ],
         ),
